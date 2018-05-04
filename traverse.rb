3a122f141afc01368f64bf92df05b593ad01741e
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-04 12:45:58 -0400

    Add logic to refactored methods

--- traverse.rb
+++ traverse.rb
@@ -5,20 +5,20 @@
 #           [13, 14, 15, 16]]
 
 # 6 x 6 matrix
-# matrix = [[1, 2, 3, 4, 5, 6],
-#           [7, 8, 9, 10, 11, 12],
-#           [13, 14, 15, 16, 17, 18],
-#           [19, 20, 21, 22, 23, 24],
-#           [25, 26, 27, 28, 29, 30],
-#           [31, 32, 33, 34, 35, 36]]
+matrix = [[1, 2, 3, 4, 5, 6],
+          [7, 8, 9, 10, 11, 12],
+          [13, 14, 15, 16, 17, 18],
+          [19, 20, 21, 22, 23, 24],
+          [25, 26, 27, 28, 29, 30],
+          [31, 32, 33, 34, 35, 36]]
 
 # 5 x 6 matrix
-matrix = [[1, 2, 3, 4, 5],
-          [6, 7, 8, 9, 10],
-          [11, 12, 13, 14, 15],
-          [16, 17, 18, 19, 20],
-          [21, 22, 23, 24, 25],
-          [26, 27, 28, 29, 30]]
+# matrix = [[1, 2, 3, 4, 5],
+#           [6, 7, 8, 9, 10],
+#           [11, 12, 13, 14, 15],
+#           [16, 17, 18, 19, 20],
+#           [21, 22, 23, 24, 25],
+#           [26, 27, 28, 29, 30]]
 
 # 2 x 4 matrix
 # matrix = [[1, 2, 3, 4,],
@@ -48,61 +48,66 @@ class SpiralTraverse
 
     (0...@width).each do |i|
 
-      if end_of_matrix?
-        traverse_top(i)
-      else
-        break
-      end
+      traverse_top(i)
 
-      if end_of_matrix?
-        traverse_right(i)
-      else break
-      end
+      traverse_right(i)
 
-      if end_of_matrix?
-        traverse_bottom(i)
-      else break
-      end
+      traverse_bottom(i)
 
-      if end_of_matrix?
-        traverse_left(i)
-      else break
-      end
+      traverse_left(i)
     end
   end
 
-end
+  private
 
-private
+  def end_of_matrix?
+    @x < @num_elements
+  end
 
-def end_of_matrix?
-  @x < @num_elements
-end
-
-def traverse_top(i)
-  (i...@width - i).each do |top|
-    puts "top #{@matrix[i][top]}"
-    @x += 1
+  def traverse_top(i)
+    if end_of_matrix?
+      (i...@width - i).each do |top|
+        puts "top #{@matrix[i][top]}"
+        @x += 1
+      end
+    else
+      return
+    end
   end
 
   def traverse_right(i)
-    (i+1..@height-1-i).each do |right|
-      puts "right #{@matrix[right][@width - 1 - i]}"
-      @x += 1
+    if end_of_matrix?
+      (i+1..@height-1-i).each do |right|
+        puts "right #{@matrix[right][@width - 1 - i]}"
+        @x += 1
+      end
+    else
+      return
     end
   end
+
   def traverse_bottom(i)
-    (i+1..@width-2-i).reverse_each do |bottom|
-      puts "bottom #{@matrix[@height-1-i][bottom]}"
-      @x += 1
+    if end_of_matrix?
+      (i+1..@width-2-i).reverse_each do |bottom|
+        puts "bottom #{@matrix[@height-1-i][bottom]}"
+        @x += 1
+      end
+    else
+      return
     end
   end
+
   def traverse_left(i)
-    (i+1..@height-1-i).reverse_each do |left|
-      puts "left #{@matrix[left][i]}"
-      @x += 1
+    if end_of_matrix?
+      (i+1..@height-1-i).reverse_each do |left|
+        puts "left #{@matrix[left][i]}"
+        @x += 1
+      end
+    else
+      return
     end
   end
+
 end
 
 spiral_traverse = SpiralTraverse.new
