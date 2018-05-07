c46d45069053bd49cc7a7ac6e051546d30023b09
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-07 16:37:03 -0400

    Edit readme and remove commented matrix / sample code from traverse.rb

--- Readme.md
+++ Readme.md
@@ -1,1 +1,23 @@
-Solution to traverse NxN matrix
+Brute-force solution to traverse MxN matrix in Ruby (provided as an array of
+arrays).
+
+This class does not modify the original array, nor does it generate a new array
+from the output, simply outputting each element to the terminal.
+
+# Usage
+
+Create a new instance of the SpiralTraverse class, and feed it an array of
+arrays!
+
+```ruby
+matrix = [[1, 2, 3, 4, 5, 6],
+          [7, 8, 9, 10, 11, 12],
+          [13, 14, 15, 16, 17, 18],
+          [19, 20, 21, 22, 23, 24],
+          [25, 26, 27, 28, 29, 30],
+          [31, 32, 33, 34, 35, 36]]
+
+spiral_traverse = SpiralTraverse.new
+
+spiral_traverse.traverse(matrix)
+```
--- traverse.rb
+++ traverse.rb
@@ -1,30 +1,3 @@
-# 5 x 5 matrix
-#matrix = [[1, 2, 3, 4],
-#           [5, 6, 7, 8],
-#           [9, 10, 11, 12],
-#           [13, 14, 15, 16]]
-
-# 6 x 6 matrix
-matrix = [[1, 2, 3, 4, 5, 6],
-          [7, 8, 9, 10, 11, 12],
-          [13, 14, 15, 16, 17, 18],
-          [19, 20, 21, 22, 23, 24],
-          [25, 26, 27, 28, 29, 30],
-          [31, 32, 33, 34, 35, 36]]
-
-# 5 x 6 matrix
-# matrix = [[1, 2, 3, 4, 5],
-#           [6, 7, 8, 9, 10],
-#           [11, 12, 13, 14, 15],
-#           [16, 17, 18, 19, 20],
-#           [21, 22, 23, 24, 25],
-#           [26, 27, 28, 29, 30]]
-
-# 2 x 4 matrix
-# matrix = [[1, 2, 3, 4,],
-#           [5, 6, 7, 8]]
-
-
 class SpiralTraverse
 
   def initialize
@@ -67,7 +40,7 @@ class SpiralTraverse
   def traverse_top(i)
     if end_of_matrix?
       (i...@width - i).each do |top|
-        puts "top #{@matrix[i][top]}"
+        puts "#{@matrix[i][top]}"
         @x += 1
       end
     else
@@ -78,7 +51,7 @@ class SpiralTraverse
   def traverse_right(i)
     if end_of_matrix?
       (i+1..@height-1-i).each do |right|
-        puts "right #{@matrix[right][@width - 1 - i]}"
+        puts "#{@matrix[right][@width - 1 - i]}"
         @x += 1
       end
     else
@@ -89,7 +62,7 @@ class SpiralTraverse
   def traverse_bottom(i)
     if end_of_matrix?
       (i+1..@width-2-i).reverse_each do |bottom|
-        puts "bottom #{@matrix[@height-1-i][bottom]}"
+        puts "#{@matrix[@height-1-i][bottom]}"
         @x += 1
       end
     else
@@ -100,7 +73,7 @@ class SpiralTraverse
   def traverse_left(i)
     if end_of_matrix?
       (i+1..@height-1-i).reverse_each do |left|
-        puts "left #{@matrix[left][i]}"
+        puts "#{@matrix[left][i]}"
         @x += 1
       end
     else
@@ -109,7 +82,3 @@ class SpiralTraverse
   end
 
 end
-
-spiral_traverse = SpiralTraverse.new
-
-spiral_traverse.traverse(matrix)
